8794994c76a03ef79e12839cf98dea3949551bdd
yehonathansc <yehonathan.sc@gmail.com>
2025-12-27 21:01:16 +0200

    pcap parser and vendor lookup working!!

---
+++ networkAnalyzer.py
@@ -0,0 +1,104 @@
+import scapy.data
+from scapy.all import rdpcap
+from scapy.layers.inet import IP
+from scapy.layers.l2 import Ether
+from mac_vendor_lookup import MacLookup
+
+
+class AnalyzeNetwork:
+    def __init__(self, pcap_path):
+        """
+        pcap_path (string): path to a pcap file
+        """
+        self.pcap_path = pcap_path
+        self.packets = rdpcap(self.pcap_path)
+
+
+    def get_ips(self):
+        """returns a list of ip addresses (strings) that appear in
+        the pcap"""
+        ips = set()
+        for packet in self.packets:
+            if "IP" in packet:
+                ips.add(packet[IP].dst)
+        return list(ips)
+
+
+    def get_macs(self):
+        """returns a list of MAC addresses (strings) that appear in
+        the pcap"""
+        macs = set()
+        for packet in self.packets:
+            eth_header = None
+            if Ether in packet:
+                eth_header = Ether
+            elif "Ethernet" in packet:
+                eth_header = "Ethernet"
+            elif "cooked linux" in packet:
+                eth_header = "cooked linux"
+            for atr in ("dst", "src"):
+                if Ether in packet:
+                    macs.add(getattr(packet[eth_header], atr))
+        return list(macs)
+
+
+    def get_info_by_mac(self, mac):
+        """returns a dict with all information about the device with
+        given MAC address"""
+        for device in self.get_info():
+            if mac == device["MAC"]:
+                return device
+        return None
+
+
+    def get_info_by_ip(self, ip):
+        """returns a dict with all information about the device with
+        given IP address"""
+        for device in self.get_info():
+            if ip == device["IP"]:
+                return device
+        return None
+
+
+    def get_info(self):
+        """returns a list of dicts with information about every
+        device in the pcap"""
+        devices = set()
+        for packet in self.packets:
+            eth_header = None
+            if Ether in packet:
+                eth_header = Ether
+            elif "Ethernet" in packet:
+                eth_header = "Ethernet"
+            elif "cooked linux" in packet:
+                eth_header = "cooked linux"
+            for atr in ("dst", "src"):
+                if eth_header != None:
+                    device = {"MAC": getattr(packet[eth_header], atr)}
+                    if device["MAC"] == "ff:ff:ff:ff:ff:ff":
+                        continue
+                    if "IP" in packet:
+                        device["IP"] = getattr(packet[IP], atr)
+                    else:
+                        device["IP"] = "UNKNOWN"
+                    try:
+                        device["VENDOR"] = MacLookup().lookup(device["MAC"])
+                    except KeyError:
+                        device["VENDOR"] = "UNKNOWN"
+                    devices.add(frozenset(device.items()))
+        return [dict(d) for d in devices]
+
+
+    def __repr__(self):
+        return "NetworkAnalayzer_for_pcap_at: " + str(self.pcap_path)
+
+
+    def __str__(self):
+        return self.__repr__()
+
+
+if __name__ == "__main__":
+    analyzer = AnalyzeNetwork("./example.pcapng")
+    print(analyzer.get_macs())
+    print(analyzer.get_ips())
+    print(analyzer.get_info())
