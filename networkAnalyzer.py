ff7a77d54e41b32fcf4560c56f3f5c1d73c9b740
yehonathansc <yehonathan.sc@gmail.com>
2026-01-03 12:19:54 +0200

    os guesser

--- networkAnalyzer.py
+++ networkAnalyzer.py
@@ -1,6 +1,6 @@
 import scapy.data
 from scapy.all import rdpcap
-from scapy.layers.inet import IP
+from scapy.layers.inet import IP, ICMP
 from scapy.layers.l2 import Ether
 from mac_vendor_lookup import MacLookup
 
@@ -96,6 +96,25 @@ class AnalyzeNetwork:
     def __str__(self):
         return self.__repr__()
 
+    def guess_os(self, device_info):
+        seqs = []
+        sum_ttl = 0
+        num = 0
+        for packet in self.packets:
+            if IP in packet:
+                if packet[IP].src == device_info["IP"]:
+                    ttl = packet[IP].ttl
+                    num += 1
+        if num == 0:
+            return ["windows", "linux", "macOS", "unix"] # no info
+        ttl_avg = sum_ttl / num
+        if ttl_avg > 128:
+            return ["unix"] # like solaris and BSD
+        if ttl_avg > 64:
+            return ["windows"] # modern windows
+        if ttl_avg > 32:
+            return ["linux", "unix", "macOS"]
+        return ["windows"] # old windows versions like windows 95
 
 if __name__ == "__main__":
     analyzer = AnalyzeNetwork("./example.pcapng")
